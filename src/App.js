068da5a9a485805be62ba0c55441f63d1e8cdf75
Yongrok Oh <ohyr96@ajou.ac.kr>
2021-05-03 22:23:08 +0900

    Mount Loading example

--- src/App.js
+++ src/App.js
@@ -2,32 +2,16 @@ import React from "react";
 
 class App extends React.Component {
   state = {
-    count: 0,
-  };
-  add = () => {
-    this.setState((current) => ({ count: current.count + 1 }));
-  };
-  minus = () => {
-    this.setState((current) => ({ count: current.count - 1 }));
+    isLoading: true,
   };
   componentDidMount() {
-    console.log("Component rendered");
-  }
-  componentDidUpdate() {
-    console.log("Updated");
-  }
-  componentWillUnmount() {
-    console.log("Good bye, cruel world");
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
   }
   render() {
-    console.log("Rendering");
-    return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
   }
 }
 
