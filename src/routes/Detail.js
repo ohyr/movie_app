504bedcc21a128e6c4ff8e33a99b76a9e35bc4ad
Yongrok Oh <ohyr96@ajou.ac.kr>
2021-05-06 18:46:26 +0900

    Movie Detail Page

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -10,7 +10,30 @@ class Detail extends React.Component {
   render() {
     const { location } = this.props;
     if (location.state) {
-      return <span>{location.state.title}</span>;
+      const title = location.state.title;
+      const poster = location.state.poster;
+      const year = location.state.year;
+      const genres = location.state.genres;
+      const summary = location.state.summary;
+      return (
+        <div className="movies">
+          <div className="movie">
+            <img src={poster} alt={title} title={title} />
+            <div className="movie__data">
+              <h3 className="movie__title">{title}</h3>
+              <h5 className="movie__year">{year}</h5>
+              <ul className="movie__genres">
+                {genres.map((genre, index) => (
+                  <li key={index} className="genres__genre">
+                    {genre}
+                  </li>
+                ))}
+              </ul>
+              <p className="movie__summary">{summary}</p>
+            </div>
+          </div>
+        </div>
+      );
     } else {
       return null;
     }
